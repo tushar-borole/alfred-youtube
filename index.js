03f1084e9c55b539ce7964bc517ffa1adf78b0b3
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-06-14 23:36:21 +0200

    Init App

---
+++ index.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const alfy = require('alfy');
+const youtubeSearch = require('youtube-search');
+
+class Alfred {
+  constructor() {
+    this.key = process.env.YOUTUBE_API_KEY;
+    this.output = [];
+  }
+
+  setOutput(item) {
+    this.output.push({
+      uid: item.id,
+      title: item.title,
+      subtitle: `${item.channelTitle} || ${item.kind
+        .split('#')[1]
+        .toUpperCase()}`,
+      arg: item.link
+    });
+  }
+
+  getOutput() {
+    if (this.output.length === 0) {
+      this.output.push({
+        title: 'No items found.'
+      });
+    }
+
+    return this.output;
+  }
+
+  hasApiKey() {
+    if (this.key.length === 0) {
+      this.output.push({
+        title: 'No API key. Press ENTER and follow the instructions',
+        arg: 'https://github.com/patrickkahl/alfred-youtube#install'
+      });
+      return false;
+    }
+
+    return true;
+  }
+
+  run() {
+    if (this.hasApiKey()) {
+      const promise = new Promise((resolve, reject) => {
+        youtubeSearch(
+          alfy.input,
+          {
+            order: 'relevance',
+            key: this.key
+          },
+          (err, results) => {
+            if (err) {
+              reject(err);
+              return;
+            }
+
+            resolve(results);
+          }
+        );
+      });
+
+      promise.then(
+        results => {
+          results.forEach(result => {
+            this.setOutput(result);
+          });
+          alfy.output(this.getOutput());
+        },
+        function(err) {
+          alfy.error(err);
+        }
+      );
+    } else {
+      alfy.output(this.getOutput());
+    }
+  }
+}
+
+const wf = new Alfred();
+wf.run();
---
+++ readme.md
@@ -0,0 +1,22 @@
+# alfred-youtube [![Build Status](https://travis-ci.org/patrickkahl/alfred-youtube.svg?branch=master)](https://travis-ci.org/patrickkahl/alfred-youtube)
+
+> Search YouTube
+
+
+## Install
+
+```
+$ npm i --g alfred-youtube
+```
+
+*Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+
+## Usage
+
+In Alfred, type `yt`, <kbd>Enter</kbd>, and your query.
+
+
+## License
+
+MIT © [Patrick Kahl](https://github.com/patrickkahl)
